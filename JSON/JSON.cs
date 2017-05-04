a8b573f48bc58ef61d0046ba058254ad59703506
hmatos <hmatos@WIN-BPMORDGW>
2017-05-04 13:18:02 +0000

    Add project files.

---
+++ JSON/JSON.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+
+    public class Rootobject
+    {
+        public Datum[] data { get; set; }
+        public Paging paging { get; set; }
+    }
+
+    public class Paging
+    {
+        public Cursors cursors { get; set; }
+    }
+
+    public class Cursors
+    {
+        public string before { get; set; }
+        public string after { get; set; }
+    }
+
+    public class Datum
+    {
+        public DateTime created_time { get; set; }
+        public From from { get; set; }
+        public string message { get; set; }
+        public bool can_remove { get; set; }
+        public int like_count { get; set; }
+        public Message_Tags[] message_tags { get; set; }
+        public bool user_likes { get; set; }
+        public string id { get; set; }
+    }
+
+    public class From
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Message_Tags
+    {
+        public string id { get; set; }
+        public int length { get; set; }
+        public string name { get; set; }
+        public int offset { get; set; }
+        public string type { get; set; }
+    }
+
+}
---
+++ JSON/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook;
+using System.Configuration;
+
+namespace JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configure fbclient and send the request
+            var client = new FacebookClient();
+            client.AccessToken = ConfigurationManager.AppSettings["sdk"];
+            client.AppId = "v2.3";
+            var get = client.Get("10155212799055040/comments?limit=500");
+            string text = Convert.ToString(get);
+
+            // Serialize fb api request
+            JavaScriptSerializer oJS = new JavaScriptSerializer();
+            Rootobject oRootObject = new Rootobject();
+            oRootObject = oJS.Deserialize<Rootobject>(text);
+            
+            // Order by likes 
+            IEnumerable<Datum> query = oRootObject.data.OrderBy(dat => dat.like_count);
+
+            // Set limit date for posting
+            string dateTime = "05/03/2017 23:59:59.42";
+            DateTime dt = Convert.ToDateTime(dateTime);
+
+            // Print the result
+            foreach (var data in query)
+            {
+                if (data.created_time < dt)
+                {
+                    Console.Write("User:" + data.from.name);
+                    Console.WriteLine("\tCreated:" + data.created_time.ToString());
+                    Console.WriteLine(data.like_count + " likes");
+                    Console.WriteLine("-------------------------------------------------------------------");
+                }
+                else
+                {
+                    Console.Write("Invalid User:" + data.from.name);
+                    Console.WriteLine("\tCreated:" + data.created_time.ToString());
+                    Console.WriteLine(data.like_count + " likes");
+                    Console.WriteLine("-------------------------------------------------------------------");
+                }
+            }
+            Console.WriteLine(oRootObject.data.Length + " Posts");
+
+            Console.Write("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}
